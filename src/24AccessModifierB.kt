559cd2f38d2ffbf3ce6cb46abb1115badcf0477c
sayfudinamry <sayfudinamry@gmail.com>
2025-10-03 09:51:43 +0700

    Acces modifier B

---
+++ src/24AccessModifierB.kt
@@ -0,0 +1,76 @@
+package id.ac.polbeng.depandi.test_kelas
+
+abstract class Vehicle(
+    val name: String,
+    val color: String,
+    val weight: Double
+) {
+    // Abstract Property (Must be overridden by Subclasses)
+    abstract var maxSpeed: Double
+
+    // Abstract Methods (Must be implemented by Subclasses)
+    abstract fun start()
+    abstract fun stop()
+    abstract fun sound()
+
+    // Concrete (Non Abstract) Method
+    fun displayDetails() {
+        println("Name: $name, Color: $color, Weight: $weight, Max Speed: $maxSpeed")
+    }
+}
+
+class Car(
+    name: String,
+    color: String,
+    weight: Double,
+    override var maxSpeed: Double
+) : Vehicle(name, color, weight) {
+
+    override fun start() {
+        println("Car Started")
+    }
+
+    override fun stop() {
+        println("Car Stopped")
+    }
+
+    override fun sound() {
+        println("Brum...brum...brumm!")
+    }
+}
+
+class Motorcycle(
+    name: String,
+    color: String,
+    weight: Double,
+    override var maxSpeed: Double
+) : Vehicle(name, color, weight) {
+
+    override fun start() {
+        println("Bike Started")
+    }
+
+    override fun stop() {
+        println("Bike Stopped")
+    }
+
+    override fun sound() {
+        println("Preng...preng...preng!")
+    }
+}
+
+fun main() {
+    val car = Car("Ferrari 812 Superfast", "red", 1525.0, 339.60)
+    val motorCycle = Motorcycle("Ducati 1098s", "red", 173.0, 271.0)
+
+    car.displayDetails()
+    motorCycle.displayDetails()
+
+    car.start()
+    car.sound()
+    car.stop()
+
+    motorCycle.start()
+    motorCycle.sound()
+    motorCycle.stop()
+}
