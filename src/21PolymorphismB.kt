89d38fac7a57ab0a0f5dea127f931452d764abb5
sayfudinamry <sayfudinamry@gmail.com>
2025-10-03 09:22:23 +0700

    Poly morphisma B

---
+++ src/21PolymorphismB.kt
@@ -0,0 +1,24 @@
+package id.ac.polbeng.depandi.test_kelas
+
+open class MyBase {
+    // Must use "open" modifier to allow child classes to override it
+    open fun think() {
+        println("Hey!! i am thinking ")
+    }
+}
+
+class MyDerived : MyBase() {
+    // inheritance happens using default constructor
+    // Must use "override" modifier to override a base class function
+    override fun think() {
+        println("I Am from Child")
+    }
+}
+
+fun main() {
+    val myBase = MyBase()
+    myBase.think()
+
+    val myDerived = MyDerived()
+    myDerived.think()
+}
