fccbe3884e16fa27cfb0b4003ff0cc7aa0e0155b
sayfudinamry <sayfudinamry@gmail.com>
2025-10-03 11:11:08 +0700

    Interface c

---
+++ src/27InterfaceC.kt
@@ -0,0 +1,36 @@
+package id.ac.polbeng.depandi.test_kelas
+
+interface X {
+    fun demoX() {
+        println("demoX function")
+    }
+
+    fun funcX()
+}
+
+interface Y {
+    fun demoY() {
+        println("demoY function")
+    }
+
+    fun funcY()
+}
+
+// Class ini mengimplementasikan interface X dan Y
+class MyClassA : X, Y {
+    override fun funcX() {
+        println("Hello")
+    }
+
+    override fun funcY() {
+        println("Hi")
+    }
+}
+
+fun main() {
+    val obj = MyClassA()
+    obj.demoX()
+    obj.demoY()
+    obj.funcX()
+    obj.funcY()
+}
