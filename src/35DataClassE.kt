06e7dee3daeaf625c48fd537958815aef0536099
sayfudinamry <sayfudinamry@gmail.com>
2025-10-03 18:35:02 +0700

    data class E

---
+++ src/35DataClassE.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.depandi.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+
+    // Destructuring Declaration
+    val (name, age) = meri
+
+    println("Name = $name")
+    println("Age = $age")
+}
